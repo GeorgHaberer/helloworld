33f90d7316d74903224825e2c27920dae4d6bf34
GeorgHaberer <georghaberer@gmail.com>
2023-12-18 11:28:27 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a my first test of github
+# just a simple helloworld script :)
+
+
+def hello():
+
+    print("Hello World!")
+    return
+
+
+if __name__ == '__main__':
+
+    hello()
+
